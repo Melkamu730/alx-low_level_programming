28bae175cc7ae4f89ff3766988c278dbf811a66a
Melkamu730 <melkamum27@gmail.com>
2023-02-23 06:05:12 -0800

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+*main- a program that finds and prints the largest prime factor of the number
+*of a number 612852475143
+*
+*Return: returns 0
+*/
+
+int main(void)
+{
+long number = 612852475143;
+int inc;
+while (inc++ < number / 2)
+{
+if (number % inc == 0)
+{
+number /= 2;
+continue;
+}
+
+for (inc = 3; inc < number / 2; inc += 2)
+{
+if (number % inc == 0)
+number /= inc;
+
+}
+}
+printf("%ld\n", number);
+return (0);
+}
