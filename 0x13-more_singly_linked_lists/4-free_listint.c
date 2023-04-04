a8e5b94ab5868c3b39f5ff89873c7372aa76abaf
Melkamu730 <melkamum27@gmail.com>
2023-04-04 04:58:53 -0700

    update free listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * free_listint - Write a function that frees a listint_t list
- * @head: My struct
+ * @head: Struct
  * Return: return to the list
  */
 void free_listint(listint_t *head)
